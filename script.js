143b163aceba863b0a3c9578a846a2d061517f0e
Pavel Kurach <pavel.kurach1@gmail.com>
2023-03-23 18:34:28 +0100

    Complete form validation

--- script.js
+++ script.js
@@ -13,26 +13,59 @@ const confirmPasswordError = document.querySelector(
 	"#confirm-password ~ .error",
 );
 
+function checkPattern(inputObject, errorObject, name, pattern) {
+	inputObject.setAttribute("class", "input-entered");
+	msg = "";
+	if (inputObject.value == "") {
+		msg = `Please, enter your ${name}`;
+	} else if (!pattern.test(inputObject.value)) {
+		msg = `Please, enter a valid ${name}`;
+	}
+	errorObject.innerText = msg;
+	inputObject.setCustomValidity(msg);
+}
+
 firstName.addEventListener("input", () => {
 	pattern = /^[a-zA-Z\s]+$/g;
-	msg = "";
-	if (firstName.value == "") {
-		msg = "Please, enter your name";
-	} else if (!pattern.test(firstName.value)) {
-		msg = "Please, enter a valid name";
-	}
-	firstNameError.innerText = msg;
-	firstName.setCustomValidity(msg);
+	checkPattern(firstName, firstNameError, "name", pattern);
 });
 
 lastName.addEventListener("input", () => {
 	pattern = /^[a-zA-Z\s]+$/g;
+	checkPattern(lastName, lastNameError, "last name", pattern);
+});
+
+email.addEventListener("input", () => {
+	pattern =
+		/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	checkPattern(email, emailError, "email", pattern);
+});
+
+number.addEventListener("input", () => {
+	pattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+	checkPattern(number, numberError, "phone number", pattern);
+});
+
+password.addEventListener("input", () => {
+	password.setAttribute("class", "input-entered");
+	pattern = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 	msg = "";
-	if (lastName.value == "") {
-		msg = "Please, enter your last name";
-	} else if (!pattern.test(lastName.value)) {
-		msg = "Please, enter a valid last name";
+	if (password.value == "") {
+		msg = "Please, enter your password";
+	} else if (!pattern.test(password.value)) {
+		msg =
+			"Password must contain at least 8 characters, with at least one symbol, upper and lower case letters and a number";
 	}
-	lastNameError.innerText = msg;
-	lastName.setCustomValidity(msg);
+	passwordError.innerText = msg;
+	password.setCustomValidity(msg);
+});
+
+confirmPassword.addEventListener("input", () => {
+	confirmPassword.setAttribute("class", "input-entered");
+	msg = "";
+	if (password.value != confirmPassword.value) {
+		msg = "Passwords do not match";
+	}
+	confirmPasswordError.innerText = msg;
+	confirmPassword.setCustomValidity(msg);
 });
